84124b16c9ad5d8639ad677a89ac9604fb19d750
Andi <andimarsituru0064@gmail.com>
2026-04-08 04:29:29 +0800

    07.Mengenal Lifecycle Hooks VueJs

--- app.js
+++ app.js
@@ -1,50 +1,15 @@
-var dataProduct = {
-    maximum: 200,
-    products: [
-        {
-        "id": "429",
-        "name": "Bamboo Thermal Ski Coat",
-        "description": "You'll be the most environmentally conscious skier on the slopes - and the most stylish - wearing our fitted bamboo thermal ski coat, made from organic bamboo with recycled plastic down filling.",
-        "image_title": "Bamboo Thermal Ski Coat",
-        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAhKpBeaDAD-RKuNgvqYtU8K1wp2EB51VtDA&s",
-        "price": "99.00"
-      },
-      {
-        "id": "430",
-        "name": "Bamboo Thermal Snowboard Jacket",
-        "description": "Stay warm and eco-friendly on the slopes with our bamboo thermal snowboard jacket, made from organic bamboo with recycled plastic down filling.",
-        "image_title": "Bamboo Thermal Snowboard Jacket",
-        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzh7hX8byNHQe21IZrHzuDkEX2yfYP_sJNSw&s",
-        "price": "149.00"
-      },
-      {
-        "id": "431",
-        "name": "Bamboo Thermal Snowboard Pants",
-        "description": "Stay warm and eco-friendly on the slopes with our bamboo thermal snowboard pants, made from organic bamboo with recycled plastic insulation.",
-        "image_title": "Bamboo Thermal Snowboard Pants",
-        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSiXfwMxHnT3jKnNngl3YLWUFOQUTjQzC1fg&s",
-        "price": "129.00"
-      },
-      {
-        "id": "432",
-        "name": "Bamboo Thermal Ski Boots",
-        "description": "Stay warm and eco-friendly on the slopes with our bamboo thermal ski boots, made from organic bamboo with recycled plastic insulation.",
-        "image_title": "Bamboo Thermal Ski Boots",
-        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSnqrYjWXWfYMAf0KCOJjqrh0Ww9ciTRt78g&s",
-        "price": "199.00"
-      },
-      {
-        "id": "433",
-        "name": "Bamboo Thermal Snowboard Gloves",
-        "description": "Stay warm and eco-friendly on the slopes with our bamboo thermal snowboard gloves, made from organic bamboo with recycled plastic insulation.",
-        "image_title": "Bamboo Thermal Snowboard Gloves",
-        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCDK7dHZ8QPGmtK2KQscwgoo6wehGsl6oJ2A&s",
-        "price": "49.00"
-      }
-    ]
-}
-
 var app = new Vue({
   el: "#app",
-  data: dataProduct
+  data: {
+    maximum: 50,
+    products: null
+  }, 
+  mounted: function() {
+    fetch('https://hplussport.com/api/products/order/price')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      this.products = data;
+    })
+  },
 });
